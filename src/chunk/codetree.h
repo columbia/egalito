402adcdc1d7943a9324b6772a9422e86e840cbae
dwk <dwks42@gmail.com>
2016-11-26 13:15:09 -0500

    Removing some old unused code.

--- src/chunk/codetree.h
+++
@@ -1,8 +0,0 @@
-#ifndef EGALITO_CODETREE_H
-#define EGALITO_CODETREE_H
-
-class CodeTree {
-
-};
-
-#endif
--- src/chunk/generation.cpp
+++
@@ -1,9 +0,0 @@
-#include "generation.h"
-#include "transform/sandbox.h"
-
-void Generation::addChunk(Chunk *chunk) {
-/*
-    auto slot = sandbox->allocate(chunk->getSize());
-    chunk->writeTo(&slot);
-*/
-}
