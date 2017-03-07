6945040dbf6685b55b8b151ed69cb9cb3af381e6
dwk <dwks42@gmail.com>
2017-03-07 15:41:44 -0500

    Testing the new ChunkMutator functionality. Seems to work well.

---
+++ test/chunk/_mutator.cpp
@@ -0,0 +1,127 @@
+#include "framework/include.h"
+#include "framework/StreamAsString.h"
+#include "chunk/mutator.h"
+#include "chunk/instruction.h"
+#include "chunk/dump.h"
+#include "disasm/disassemble.h"
+#include "pass/chunkpass.h"
+#include "log/registry.h"
+
+static Instruction *makeWithImmediate(unsigned char imm) {
+#ifdef ARCH_X86_64
+    // add imm, %eax
+    std::vector<unsigned char> bytes = {0x83, 0xc0, imm};
+#else
+    #error "not ported to ARM yet"
+#endif
+
+    return Disassemble::instruction(bytes, true, 0);
+}
+
+static Block *makeBlock() {
+    PositionFactory *positionFactory = PositionFactory::getInstance();
+    Block *block = new Block();
+    block->setPosition(
+        positionFactory->makePosition(nullptr, block, 0));
+    return block;
+}
+
+static void ensureValues(Block *block, const std::vector<unsigned char> &values) {
+    size_t index = 0;
+    for(auto ins : block->getChildren()->getIterable()->iterable()) {
+        CAPTURE(ins->getName());  // debug info, print instr name
+        CHECK(ins->getSemantic()->getData()[2] == values[index]);
+        index ++;
+    }
+}
+
+TEST_CASE("calling append() on ChunkMutator", "[chunk][normal]") {
+    Disassemble::init();
+    PositionFactory *positionFactory = PositionFactory::getInstance();
+
+    Block *block = makeBlock();
+
+    Chunk *prevChunk = nullptr;
+    for(unsigned char c = 1; c <= 3; c ++) {
+        auto instr = makeWithImmediate(c);
+        instr->setPosition(
+            positionFactory->makePosition(prevChunk, instr, block->getSize()));
+        ChunkMutator(block).append(instr);
+
+        prevChunk = instr;
+    }
+
+    ensureValues(block, {1, 2, 3});
+    delete block;
+}
+
+TEST_CASE("calling insert functions with ChunkMutator", "[chunk][normal]") {
+    Disassemble::init();
+    PositionFactory *positionFactory = PositionFactory::getInstance();
+
+    Block *block = makeBlock();
+
+    SECTION("empty list") {
+        SECTION("call prepend()") {
+            auto instr = makeWithImmediate(22);
+            instr->setPosition(positionFactory->makePosition(block, instr, 0));
+            ChunkMutator(block).prepend(instr);
+            ensureValues(block, {22});
+        }
+    }
+
+    SECTION("non-empty list") {
+        Chunk *prevChunk = nullptr;
+        for(unsigned char c = 1; c <= 3; c ++) {
+            auto instr = makeWithImmediate(c);
+            instr->setPosition(
+                positionFactory->makePosition(prevChunk, instr, block->getSize()));
+            ChunkMutator(block).append(instr);
+
+            prevChunk = instr;
+        }
+
+        ensureValues(block, {1, 2, 3});
+
+        SECTION("call prepend()") {
+            auto instr = makeWithImmediate(22);
+            instr->setPosition(positionFactory->makePosition(block, instr, 0));
+            ChunkMutator(block).prepend(instr);
+            ensureValues(block, {22, 1, 2, 3});
+        }
+
+        SECTION("call insertAfter()") {
+            auto point = block->getChildren()->genericGetAt(0);  // gives the '1'
+            auto instr = makeWithImmediate(44);
+            instr->setPosition(positionFactory->makePosition(point, instr, 0));
+            ChunkMutator(block).insertAfter(point, instr);
+            ensureValues(block, {1, 44, 2, 3});
+        }
+
+        SECTION("call insertBefore()") {
+            auto point1 = block->getChildren()->genericGetAt(0);  // gives the '1'
+            auto point2 = block->getChildren()->genericGetAt(1);  // gives the '2'
+            auto instr = makeWithImmediate(44);
+            instr->setPosition(positionFactory->makePosition(point1, instr, 0));
+            ChunkMutator(block).insertBefore(point2, instr);
+            ensureValues(block, {1, 44, 2, 3});
+        }
+
+        SECTION("call insertAfter() with NULL point") {
+            auto instr = makeWithImmediate(44);
+            instr->setPosition(positionFactory->makePosition(block, instr, 0));
+            ChunkMutator(block).insertAfter(nullptr, instr);
+            ensureValues(block, {44, 1, 2, 3});
+        }
+
+        SECTION("call insertBefore() with NULL point") {
+            auto point3 = block->getChildren()->genericGetLast();  // gives the '3'
+            auto instr = makeWithImmediate(44);
+            instr->setPosition(positionFactory->makePosition(point3, instr, 0));
+            ChunkMutator(block).insertBefore(nullptr, instr);
+            ensureValues(block, {1, 2, 3, 44});
+        }
+    }
+
+    delete block;
+}
