5162d8532eb37a49430dffc6c6988a6b1d43bba2
Miguel Arroyo <miguel@arroyo.me>
2017-03-18 14:44:49 -0400

    Cross Compile Support

--- env.mk
+++ env.mk
@@ -1,24 +1,43 @@
 # Setup for egalito compilation environment
 
-CC  = gcc
-CXX = g++
+CC  = $(CROSS)gcc
+CXX = $(CROSS)g++
+
 GENERIC_FLAGS   = -Wall -Wextra -Wno-unused-parameter -I.
+
+ifneq ($(CROSS),)
+ifneq ($(CAPSTONE_INC),)
+	GENERIC_FLAGS += -isystem $(CAPSTONE_INC)
+else
+	$(error Capstone Include directory not defined)
+endif
+ifneq ($(CAPSTONE_LIB),)
+	CROSSLD = -L $(CAPSTONE_LIB)
+else
+	$(error Capstone Lib directory not defined)
+endif
+endif
+
 OPT_FLAGS       = -g -O2
 DEPFLAGS        = -MT '$@ $(@:.o=.d)' -MMD -MP
 CFLAGS          = -std=gnu99 $(GENERIC_FLAGS) $(OPT_FLAGS)
 CXXFLAGS        = -std=c++11 $(GENERIC_FLAGS) $(OPT_FLAGS)
-CLDFLAGS        = -lcapstone -Wl,-q
-#LDFLAGS         = -lcapstone -q
-#LDFLAGS2        = `gcc --print-file-name=libstdc++.a` `gcc --print-file-name=libgcc.a`
-#LDFLAGS2        = $(shell g++ -print-search-dirs 2>&1 | grep libraries | sed 's/libraries: =/:/; s/:/ -L/g') -lstdc++ --start-group -lgcc -lgcc_eh -lc --end-group
+CLDFLAGS        = $(CROSSLD) -lcapstone -Wl,-q
 
 ifdef PROFILE  # set PROFILE=1 to enable gprof profiling
-    CFLAGS += -no-pie -pg
-    CXXFLAGS += -no-pie -pg
-    CLDFLAGS += -no-pie -pg
+	CFLAGS += -no-pie -pg
+	CXXFLAGS += -no-pie -pg
+	CLDFLAGS += -no-pie -pg
 endif
 
-P_ARCH := $(shell uname -m)
+ifneq ($(CROSS),)
+	P_ARCH=$(strip $(shell echo $(CC) | awk -F- '{print $$1}'))
+else
+	P_ARCH=$(shell uname -m)
+endif
+
+$(info "Building for $(P_ARCH)")
+
 ifeq (aarch64,$(P_ARCH))
 	CFLAGS += -DARCH_AARCH64
 	CXXFLAGS += -DARCH_AARCH64
@@ -30,16 +49,15 @@ else ifeq (x86_64,$(P_ARCH))
 else
 $(error Unsupported platform, we only handle aarch64 and x86_64)
 endif
+
 CFLAGS += '-DTESTDIR="example/$(BUILDDIR)"'
 CXXFLAGS += '-DTESTDIR="example/$(BUILDDIR)"'
 
-ifneq (,$(wildcard /usr/lib/x86_64-linux-gnu))
-	GLIBCDIR = /usr/lib/x86_64-linux-gnu
-else ifneq (,$(wildcard /usr/lib/aarch64-linux-gnu))
-	GLIBCDIR = /usr/lib/aarch64-linux-gnu
-else ifneq (,$(wildcard /usr/lib64))
-	GLIBCDIR = /usr/lib64
-endif
-#STARTFILES = $(GLIBCDIR)/crt1.o $(GLIBCDIR)/crti.o `gcc --print-file-name=crtbeginT.o`
-STARTFILES = $(GLIBCDIR)/crti.o `gcc --print-file-name=crtbegin.o`
-ENDFILES = `gcc --print-file-name=crtend.o` $(GLIBCDIR)/crtn.o
+GLIBCDIR = $(dirname $(shell $(CC) --print-file-name=libc.so))
+CRTI = $(shell $(CC) --print-file-name=crti.o)
+CRTBEGIN = $(shell $(CC) --print-file-name=crtbegin.o)
+CRTEND = $(shell $(CC) --print-file-name=crtend.o)
+CRTN = $(shell $(CC) --print-file-name=crtn.o)
+
+STARTFILES = $(CRTI) $(CRTBEGIN)
+ENDFILES = $(CRTEND) $(CRTN)
