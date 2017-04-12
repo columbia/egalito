254afd5d8868994be46ee4b8f559989ed32724b6
dwk <dwks42@gmail.com>
2017-04-12 14:00:29 -0400

    Remove some old code, add comment about InferLinksPass.

--- src/pass/inferlinks.cpp
+++ src/pass/inferlinks.cpp
@@ -16,64 +16,13 @@ void InferLinksPass::visit(Instruction *instruction) {
         if(!assembly) return;
 
 #ifdef ARCH_X86_64
+        // see if this instruction has any operands that need links
+        // (can return NULL if not)
         auto linked = LinkedInstruction::makeLinked(module, instruction, assembly);
         if(linked) {
             instruction->setSemantic(linked);
             delete v;
         }
-#if 0
-        auto asmOps = assembly->getAsmOperands();
-        for(size_t i = 0; i < asmOps->getOpCount(); i ++) {
-            const cs_x86_op *op = &asmOps->getOperands()[i];
-            if(MakeSemantic::isRIPRelative(assembly, i)) {
-                address_t target = (instruction->getAddress() + instruction->getSize()) + op->mem.disp;
-                auto inferred = new LinkedInstruction(instruction, v->moveStorageFrom(), i);
-
-                auto found = CIter::spatial(module->getFunctionList())
-                    ->find(target);
-                if(found) {
-                    inferred->setLink(new NormalLink(found));
-                    instruction->setSemantic(inferred);
-                    delete v;
-
-#if 0
-                    LOG(8, "inferred function pointer at " << instruction->getAddress()
-                        << " -> " << target << ":");
-                    ChunkDumper d;
-                    instruction->accept(&d);
-#endif
-                }
-                else {
-                    inferred->setLink(new DataOffsetLink(elf, target));
-                    instruction->setSemantic(inferred);
-                    delete v;
-#if 0
-                    LOG(8, "inferred data pointer at " << instruction->getAddress()
-                        << " -> " << target << ":");
-                    ChunkDumper d;
-                    instruction->accept(&d);
-#endif
-                }
-                return;
-            }
-            else if(op->type == X86_OP_IMM) {
-                address_t target = op->imm;
-                auto found = CIter::spatial(module->getFunctionList())
-                    ->find(target);
-                if(found) {
-                    auto inferred = new AbsoluteLinkedInstruction(
-                        instruction, v->moveStorageFrom(), i);
-                    inferred->setLink(new ExternalNormalLink(found));
-                    instruction->setSemantic(inferred);
-                    delete v;
-                    return;  // don't access v after we delete it
-                }
-                /*else {
-                    inferred->setLink(new DataOffsetLink(elf, target));
-                }*/
-            }
-        }
-#endif
 #elif defined(ARCH_AARCH64)
         auto linked = LinkedInstruction::makeLinked(module, instruction, assembly);
         if(linked) {
