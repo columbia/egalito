d60e496ff759cb9b8db15239ba2b821f42f2562b
Miguel Arroyo <miguel@arroyo.me>
2017-04-13 17:50:48 -0400

    Extends Basic Python Shell.
    disassemble is still partially implemented.

--- app/python/bindings.cpp
+++ app/python/bindings.cpp
@@ -1,10 +1,42 @@
 #include <boost/python.hpp>
 #include "conductor/setup.h"
+#include "operation/find2.h"
+#include "conductor/conductor.h"
+#include "elf/symbol.h"
+#include "chunk/function.h"
+#include "types.h"
 
 BOOST_PYTHON_MODULE(python_egalito)
 {
 	using namespace boost::python;
 
- class_<ConductorSetup>("ConductorSetup")
-		.def("parseElfFiles", &ConductorSetup::parseElfFiles);
+	enum_<Symbol::SymbolType>("SymbolType")
+		.value("FUNC", Symbol::SymbolType::TYPE_FUNC)
+		.value("IFUNC", Symbol::SymbolType::TYPE_IFUNC)
+		.value("OBJECT", Symbol::SymbolType::TYPE_OBJECT)
+		.value("SECTION", Symbol::SymbolType::TYPE_SECTION)
+		.value("FILE", Symbol::SymbolType::TYPE_FILE)
+		.value("UNKNOWN", Symbol::SymbolType::TYPE_UNKNOWN);
+
+	enum_<Symbol::BindingType>("SymbolType")
+		.value("LOCAL", Symbol::BindingType::BIND_LOCAL)
+		.value("GLOBAL", Symbol::BindingType::BIND_GLOBAL)
+		.value("WEAK", Symbol::BindingType::BIND_WEAK);
+
+
+	class_<Symbol>("Symbol", init<address_t, size_t, const char *, Symbol::SymbolType, Symbol::BindingType, size_t, size_t>())
+		.def("getName", &Symbol::getName);
+
+	class_<Function>("Function", init<Symbol *>())
+		.def("getName", &Function::getName);
+
+	class_<Conductor>("Conductor");
+
+	class_<ConductorSetup>("ConductorSetup")
+		.def("parseElfFiles", &ConductorSetup::parseElfFiles)
+		.def("getConductor", &ConductorSetup::getConductor, return_value_policy<manage_new_object>());
+
+	class_<ChunkFind2>("ChunkFind2", init<Conductor *>())
+		.def("findFunction", &ChunkFind2::findFunction, arg("module")=NULL, return_value_policy<manage_new_object>());
+
 }
--- app/python/shell.py
+++ app/python/shell.py
@@ -1,15 +1,39 @@
 #!/usr/bin/env python
 import python_egalito
 import cmd
+import readline
+import sys
 
 class EgalitoShell(cmd.Cmd):
 
     prompt = 'etshell> '
+    def __init__(self, conductorSetup):
+        self.conductorSetup = conductorSetup
+        super().__init__()
 
     def do_parse(self, line):
-        conductor = python_egalito.ConductorSetup()
-        conductor.parseElfFiles(line, False, False)
+        self.conductorSetup.parseElfFiles(line, False, False)
+
+    def do_disass(self, line):
+        """TODO: Not complete yet"""
+        conductor = self.conductorSetup.getConductor()
+        chunkFind = python_egalito.ChunkFind2(conductor)
+        func = chunkFind.findFunction(line, None)
+        print(func.getName())
+
+    def do_EOF(self, line):
         return True
 
+    def cmdloop_with_keyboard_interrupt(self):
+        doQuit = False
+        while doQuit != True:
+            try:
+                self.cmdloop()
+                doQuit = True
+            except KeyboardInterrupt:
+                sys.stdout.write('\n')
+
 if __name__ == '__main__':
-    EgalitoShell().cmdloop()
+
+    conductorSetup = python_egalito.ConductorSetup()
+    EgalitoShell(conductorSetup).cmdloop_with_keyboard_interrupt()
