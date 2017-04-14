3c805d8b86ac125702c6c2574f9a0d768c01caa1
Miguel Arroyo <miguel@arroyo.me>
2017-04-13 21:20:04 -0400

    PEP8 Style.

--- app/python/shell.py
+++ app/python/shell.py
@@ -1,50 +1,52 @@
 #!/usr/bin/env python
-import python_egalito
+import python_egalito as eg
 import cmd
 import readline
 import sys
 import time
 import os
 
+
 class EgalitoShell(cmd.Cmd):
 
     prompt = 'etshell> '
-    def __init__(self, conductorSetup):
-        self.conductorSetup = conductorSetup
-        self.histfile = os.path.expanduser('.etshell_history')
-        self.histfile_size = 1000
+
+    def __init__(self, conductor_setup):
+        self.__conductor_setup = conductor_setup
+        self.__histfile = os.path.expanduser('.etshell_history')
+        self.__histfile_size = 1000
         super().__init__()
 
     def preloop(self):
-        if os.path.exists(self.histfile):
-            readline.read_history_file(self.histfile)
+        if os.path.exists(self.__histfile):
+            readline.read_history_file(self.__histfile)
 
     def postloop(self):
-        readline.set_history_length(self.histfile_size)
-        readline.write_history_file(self.histfile)
+        readline.set_history_length(self.__histfile_size)
+        readline.write_history_file(self.__histfile)
 
     def do_parse(self, line):
-        self.conductorSetup.parseElfFiles(line, False, False)
+        self.__conductor_setup.parse_elf_files(line, False, False)
 
     def do_parse2(self, line):
-        self.conductorSetup.parseElfFiles(line, True, False)
+        self.__conductor_setup.parse_elf_files(line, True, False)
 
     def do_parse3(self, line):
-        self.conductorSetup.parseElfFiles(line, True, True)
+        self.__conductor_setup.parse_elf_files(line, True, True)
 
     def do_disass(self, line):
-        conductor = self.conductorSetup.getConductor()
-        chunkFind = python_egalito.ChunkFind2(conductor)
-        func = chunkFind.findFunction(line, None)
-        chunkDumper = python_egalito.ChunkDumper()
+        conductor = self.__conductor_setup.get_conductor()
+        chunk_find = eg.ChunkFind2(conductor)
+        func = chunk_find.find_function(line, None)
+        chunk_dumper = eg.ChunkDumper()
         if (func):
-            func.accept(chunkDumper)
+            func.accept(chunk_dumper)
         else:
             print("%s not found" % (line))
 
     def do_reassign(self, line):
-        self.conductorSetup.makeLoaderSandbox()
-        self.conductorSetup.moveCodeAssignAddresses()
+        self.__conductor_setup.make_loader_sandbox()
+        self.__conductor_setup.move_code_assign_addresses()
 
     def do_q(self, line):
         return True
@@ -53,15 +55,15 @@ class EgalitoShell(cmd.Cmd):
         return True
 
     def cmdloop_with_keyboard_interrupt(self):
-        doQuit = False
-        while doQuit != True:
+        do_quit = False
+        while do_quit != True:
             try:
                 self.cmdloop()
-                doQuit = True
+                do_quit = True
             except KeyboardInterrupt:
                 sys.stdout.write('\n')
 
 if __name__ == '__main__':
 
-    conductorSetup = python_egalito.ConductorSetup()
-    EgalitoShell(conductorSetup).cmdloop_with_keyboard_interrupt()
+    conductor_setup = eg.ConductorSetup()
+    EgalitoShell(conductor_setup).cmdloop_with_keyboard_interrupt()
