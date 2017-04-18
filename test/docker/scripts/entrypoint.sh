2edb6088fef37cd8e1363d086923e21e85f5c97a
Miguel Arroyo <miguel@arroyo.me>
2017-04-18 11:59:20 -0400

    Travis CI Build Support
    Run x86_64 build using Docker image.
    Cross-compile for aarch64 on x86_64 using Docker image.

--- test/docker/scripts/entrypoint.sh
+++ test/docker/scripts/entrypoint.sh
@@ -12,5 +12,7 @@ usermod -a -G dialout user
 export HOME=/home/user
 mkdir -p $HOME/.ccache
 chown -R $USER_ID $HOME/.ccache
+export PATH=/gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu/bin:$PATH
+echo "PATH=/gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu/bin:$PATH" >> $HOME/.bashrc
 
 exec gosu user "$@"
