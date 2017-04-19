0f3f53de35f128cd3ac6e508c3d1f70f9b6f2c3d
dwk <dwks42@gmail.com>
2017-04-19 18:03:22 -0400

    Add python README. Reformat bindings.cpp.

--- app/python/bindings.cpp
+++ app/python/bindings.cpp
@@ -10,16 +10,13 @@
 
 using namespace boost::python;
 
-
-BOOST_PYTHON_MODULE(python_egalito)
-{
+BOOST_PYTHON_MODULE(python_egalito) {
     register_exception_translator<const char *>([] (const char *s) {
         PyErr_SetString(PyExc_RuntimeError, s);
     });
     register_exception_translator<std::string>([] (const std::string &s) {
         PyErr_SetString(PyExc_RuntimeError, s.c_str());
     });
-
 
 	enum_<Symbol::SymbolType>("SymbolType")
 		.value("FUNC",    Symbol::SymbolType::TYPE_FUNC)
@@ -34,14 +31,10 @@ BOOST_PYTHON_MODULE(python_egalito)
 		.value("GLOBAL", Symbol::BindingType::BIND_GLOBAL)
 		.value("WEAK",   Symbol::BindingType::BIND_WEAK);
 
-
-	class_<Symbol>("Symbol", init<
-								 address_t, size_t,
-								 const char *,
-								 Symbol::SymbolType,
-								 Symbol::BindingType,
-								 size_t,
-								 size_t>())
+	class_<Symbol>("Symbol",
+            init<address_t, size_t, const char *,
+                Symbol::SymbolType, Symbol::BindingType,
+                size_t, size_t>())
 		.def("get_name", &Symbol::getName);
 
 	class_<Function>("Function", init<Symbol *>())
@@ -52,8 +45,7 @@ BOOST_PYTHON_MODULE(python_egalito)
 
 	class_<ConductorSetup>("ConductorSetup")
 		.def("parse_elf_files",            &ConductorSetup::parseElfFiles)
-		.def("get_conductor",              &ConductorSetup::getConductor,
-																												return_internal_reference<>())
+		.def("get_conductor",              &ConductorSetup::getConductor, return_internal_reference<>())
 		.def("make_loader_sandbox",        &ConductorSetup::makeLoaderSandbox)
 		.def("move_code_assign_addresses", &ConductorSetup::moveCodeAssignAddresses);
 
@@ -63,9 +55,8 @@ BOOST_PYTHON_MODULE(python_egalito)
 
 	class_<ChunkFind2>("ChunkFind2", init<Conductor *>())
 		.def("find_function",            &ChunkFind2::findFunction,
-																									arg("module")=NULL,
-																									return_value_policy<reference_existing_object>())
+            arg("module")=NULL, return_value_policy<reference_existing_object>())
 		.def("find_function_containing", &ChunkFind2::findFunctionContaining,
-																									return_value_policy<reference_existing_object>());
+            return_value_policy<reference_existing_object>());
 
 }
