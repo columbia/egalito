74a71a18d67e96dcf12d923e27d4fc83852cb232
dwk <dwks42@gmail.com>
2017-05-05 15:46:21 -0400

    Test case that shows objgen does not handle global variables yet.

---
+++ test/example/global2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int var_data = 42;
+int var_bss;
+const int var_rodata = 55;
+
+int main() {
+    printf("var_data    = %d\n", var_data);
+    printf("var_bss     = %d\n", var_bss);
+    printf("var_rodata  = %d\n", var_rodata);
+    return 0;
+}
