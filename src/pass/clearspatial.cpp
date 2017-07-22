299947f4094e83364e0ff289d4059eb4bd5246bd
Hidenori <hidenori.kobayashi@gmail.com>
2017-07-22 10:48:18 -0400

    ClearSpatialPass clears spatial cache from chunks.

---
+++ src/pass/clearspatial.cpp
@@ -0,0 +1,15 @@
+#include "clearspatial.h"
+
+void ClearSpatialPass::visit(FunctionList *functionList) {
+    functionList->getChildren()->clearSpatial();
+    recurse(functionList);
+}
+
+void ClearSpatialPass::visit(Function *function) {
+    function->getChildren()->clearSpatial();
+    recurse(function);
+}
+
+void ClearSpatialPass::visit(Block *block) {
+    block->getChildren()->clearSpatial();
+}
---
+++ src/pass/clearspatial.h
@@ -0,0 +1,14 @@
+#ifndef EGALITO_PASS_CLEARSPATIAL_H
+#define EGALITO_PASS_CLEARSPATIAL_H
+
+#include "chunkpass.h"
+
+class ClearSpatialPass : public ChunkPass {
+private:
+public:
+    virtual void visit(FunctionList *functionList);
+    virtual void visit(Function *function);
+    virtual void visit(Block *block);
+};
+
+#endif
