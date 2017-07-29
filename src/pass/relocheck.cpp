26f6e837d1142876bfba6211e3cf7651f7b3c11f
Hidenori <hidenori.kobayashi@gmail.com>
2017-07-29 12:08:11 -0400

    Check data links.

--- src/pass/relocheck.cpp
+++ src/pass/relocheck.cpp
@@ -9,26 +9,28 @@ void ReloCheckPass::visit(Module *module) {
 #if defined(ARCH_AARCH64) || defined(ARCH_ARM)
     for(auto r : *relocList) {
         if(0
-           || (r->getType() == R_AARCH64_LD_PREL_LO19)          // ld(literal)
-           || (r->getType() == R_AARCH64_ADR_PREL_LO21)         // adr -- not yet implemented
-           || (r->getType() == R_AARCH64_ADR_PREL_PG_HI21)      // adrp
-           || (r->getType() == R_AARCH64_ADR_PREL_PG_HI21_NC)   // adrp
-           || (r->getType() == R_AARCH64_ADR_GOT_PAGE)          // adrp for GOT
-           || (r->getType() == R_AARCH64_JUMP26)                // (usually) tail call
-           || (r->getType() == R_AARCH64_CALL26)                // bl
+            || (r->getType() == R_AARCH64_LD_PREL_LO19)         // ld(literal)
+            || (r->getType() == R_AARCH64_ADR_PREL_LO21)        // adr -- not yet implemented
+            || (r->getType() == R_AARCH64_ADR_PREL_PG_HI21)     // adrp
+            || (r->getType() == R_AARCH64_ADR_PREL_PG_HI21_NC)  // adrp
+            || (r->getType() == R_AARCH64_ADR_GOT_PAGE)         // adrp for GOT
+            || (r->getType() == R_AARCH64_JUMP26)               // (usually) tail call
+            || (r->getType() == R_AARCH64_CALL26)               // bl
 
-           || (r->getType() == R_AARCH64_ABS64)                 // function pointer data
+            || (r->getType() == R_AARCH64_ABS64)                // function pointer data
+
+            || (r->getType() == R_AARCH64_ADD_ABS_LO12_NC)      // pointers to data
+            || (r->getType() == R_AARCH64_LDST8_ABS_LO12_NC)
+            || (r->getType() == R_AARCH64_LDST16_ABS_LO12_NC)
+            || (r->getType() == R_AARCH64_LDST32_ABS_LO12_NC)
+            || (r->getType() == R_AARCH64_LDST64_ABS_LO12_NC)
+            || (r->getType() == R_AARCH64_LD64_GOT_LO12_NC)
            ) {
+            if(module->getName() == "module-(executable)")
             checkSemantic(r, module->getFunctionList());
         }
         else {
             if (1
-                //should be same unless data (inc. GOT) moves
-                && (r->getType() != R_AARCH64_ADD_ABS_LO12_NC)
-                && (r->getType() != R_AARCH64_LDST8_ABS_LO12_NC)
-                && (r->getType() != R_AARCH64_LDST64_ABS_LO12_NC)
-                && (r->getType() != R_AARCH64_LD64_GOT_LO12_NC)
-
                 //related to GOT & PLT
                 && (r->getType() != R_AARCH64_GLOB_DAT)
                 && (r->getType() != R_AARCH64_JUMP_SLOT)
@@ -48,7 +50,17 @@ void ReloCheckPass::visit(Module *module) {
 void ReloCheckPass::checkSemantic(Reloc *r, FunctionList *list) {
     Chunk *inner = ChunkFind().findInnermostInsideInstruction(list, r->getAddress());
     if(auto i = dynamic_cast<Instruction *>(inner)) {
-        if(!dynamic_cast<LinkedInstruction *>(i->getSemantic())) {
+        if(auto v = dynamic_cast<LinkedInstruction *>(i->getSemantic())) {
+            LOG0(10, "relocation at " << std::hex << r->getAddress());
+            if(dynamic_cast<UnresolvedLink *>(v->getLink())) {
+                LOG(1, " NOT resolved! addend " << r->getAddend());
+            }
+            else {
+                LOG(10, " resolved to " << v->getLink()->getTarget()->getName()
+                    << " (" << v->getLink()->getTargetAddress() << ")");
+            }
+        }
+        else {
             LOG(0, i->getName() << " is still a normal DisassembledInstruction :(");
         }
     }
