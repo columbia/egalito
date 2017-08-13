5d3954b1a591cbf6d11ee9f0f6109b6fa546ff79
Hidenori <hidenori.kobayashi@gmail.com>
2017-08-13 14:12:06 -0400

    Make BinGen handle bss properly.

--- src/generate/bingen.cpp
+++ src/generate/bingen.cpp
@@ -15,6 +15,7 @@
 #include "pass/fixdataregions.h"
 #include "pass/instrumentcalls.h"
 #include "pass/switchcontext.h"
+#include "pass/relocheck.h"
 
 #include "log/log.h"
 #include "chunk/dump.h"
@@ -22,23 +23,6 @@
 #define ROUND_DOWN(x)       ((x) & ~0xfff)
 #define ROUND_UP(x)         (((x) + 0xfff) & ~0xfff)
 #define ROUND_UP_BY(x, y)   (((x) + (y) - 1) & ~((y) - 1))
-
-BinGen::BinGen(ConductorSetup *setup, const char *filename)
-    : setup(setup), mainModule(nullptr), addon(nullptr),
-      fs(filename, std::ios::out | std::ios::binary) {
-
-    mainModule = setup->getConductor()->getProgram()->getMain();
-    for(auto module : CIter::children(setup->getConductor()->getProgram())) {
-        if(module->getName() == "module-(addon)") {
-            addon = module;
-            break;
-        }
-    }
-}
-
-BinGen::~BinGen() {
-    fs.close();
-}
 
 static std::vector<Function *> makeSortedFunctionList(Module *module) {
     std::vector<Function *> list;
@@ -52,11 +36,99 @@ static std::vector<Function *> makeSortedFunctionList(Module *module) {
     return list;
 }
 
+BinGen::BinGen(ConductorSetup *setup, const char *filename)
+    : setup(setup), mainModule(nullptr), addon(nullptr),
+      fs(filename, std::ios::out | std::ios::binary) {
+
+    mainModule = setup->getConductor()->getProgram()->getMain();
+    moduleList.push_back(mainModule);
+    for(auto module : CIter::children(setup->getConductor()->getProgram())) {
+        if(module->getName() == "module-(addon)") {
+            addon = module;
+            moduleList.push_back(addon);
+            break;
+        }
+    }
+}
+
+BinGen::~BinGen() {
+    fs.close();
+}
+
+void BinGen::addBssClear() {
+    if(!addon) return;
+
+#ifdef ARCH_AARCH64
+    auto clearFunction = CIter::named(addon->getFunctionList())
+        ->find("egalito_clear_addon_bss");
+    if(!clearFunction) return;
+
+    auto startFunction = CIter::named(mainModule->getFunctionList())
+        ->find("_start");
+    if(!startFunction) return;
+
+    SwitchContextPass switcher;
+    clearFunction->accept(&switcher);
+
+    InstrumentCallsPass instrument;
+    instrument.setEntryAdvice(clearFunction);
+
+    startFunction->accept(&instrument);
+#endif
+}
+
+
+// may be this should be a pass
+void BinGen::extractLinkerSymbols() {
+    LOG(1, "extracting linker symbols");
+    // we only need to care about those without variables or chunks
+}
+
+int BinGen::generate() {
+    changeMapAddress(mainModule, 0xa0000000);
+    SegMap::mapAllSegments(setup);
+    RelocDataPass relocData(setup->getConductor());
+    setup->getConductor()->getProgram()->accept(&relocData);
+
+    ReloCheckPass checker;
+    setup->getConductor()->getProgram()->accept(&checker);
+
+    // this must be performed before any instrumentation/transformation
+    extractLinkerSymbols();
+
+    applyAdditionalTransform();
+
+    auto endOfText = reassignFunctionAddress();
+
+    address_t pos = makeImageBox();
+    //mainModule->getElfSpace()->getElfMap()->setBaseAddress(0);
+    interleaveData(endOfText);
+
+    FixDataRegionsPass fixDataRegions;
+    setup->getConductor()->getProgram()->accept(&fixDataRegions);
+
+    writeOut(pos);
+
+    setup->getConductor()->writeDebugElf("bin-symbols.elf");
+
+    LOG(0, "entry point is located at 0x"
+        << std::hex << setup->getEntryPoint());
+
+    return 0;
+}
+
+void BinGen::applyAdditionalTransform() {
+    addCallLogging();
+    addBssClear();
+
+    dePLT();
+}
+
 address_t BinGen::reassignFunctionAddress() {
     auto list = makeSortedFunctionList(mainModule);
     auto address = list.front()->getAddress();
     for(auto func : list) {
-        LOG(1, func->getName() << " : "
+        LOG(1, func->getName() << " : " << std::hex
             << func->getAddress() << " -> " << address);
         ChunkMutator(func).setPosition(address);
         address += func->getSize();
@@ -73,30 +145,6 @@ address_t BinGen::reassignFunctionAddress() {
     }
 
     return list.back()->getAddress() + list.back()->getSize();
-}
-
-void BinGen::dePLT(void) {
-    if(addon) {
-        for(auto func : CIter::functions(addon)) {
-            for(auto block : CIter::children(func)) {
-                for(auto instr : CIter::children(block)) {
-                    auto s = instr->getSemantic();
-                    auto cfi = dynamic_cast<ControlFlowInstruction *>(s);
-                    if(!cfi) continue;
-                    auto link = dynamic_cast<PLTLink *>(cfi->getLink());
-                    if(!link) continue;
-                    auto target = link->getPLTTrampoline()->getTarget();
-                    if(!target) {
-                        LOG(1, "target is supposed to be resolved!");
-                        throw "dePLT: error";
-                    }
-                    auto newLink = new NormalLink(target);
-                    cfi->setLink(newLink);
-                    delete link;
-                }
-            }
-        }
-    }
 }
 
 void BinGen::addCallLogging() {
@@ -139,42 +187,30 @@ void BinGen::addCallLogging() {
 #endif
 }
 
-void BinGen::applyAdditionalTransform() {
-    addCallLogging();
-
-    dePLT();
+void BinGen::dePLT(void) {
+    if(addon) {
+        for(auto func : CIter::functions(addon)) {
+            for(auto block : CIter::children(func)) {
+                for(auto instr : CIter::children(block)) {
+                    auto s = instr->getSemantic();
+                    auto cfi = dynamic_cast<ControlFlowInstruction *>(s);
+                    if(!cfi) continue;
+                    auto link = dynamic_cast<PLTLink *>(cfi->getLink());
+                    if(!link) continue;
+                    auto target = link->getPLTTrampoline()->getTarget();
+                    if(!target) {
+                        LOG(1, "target is supposed to be resolved!");
+                        throw "dePLT: error";
+                    }
+                    auto newLink = new NormalLink(target);
+                    cfi->setLink(newLink);
+                    delete link;
+                }
+            }
+        }
+    }
 }
 
-int BinGen::generate() {
-    applyAdditionalTransform();
-
-    auto endOfText = reassignFunctionAddress();
-    changeMapAddress(mainModule, 0xa0000000);
-
-    address_t pos = makeImageBox();
-
-    SegMap::mapAllSegments(setup);
-
-    //setup->getConductor()->fixDataSections();
-    RelocDataPass relocData(setup->getConductor());
-    setup->getConductor()->getProgram()->accept(&relocData);
-
-    //changeMapAddress(mainModule, 0);
-    mainModule->getElfSpace()->getElfMap()->setBaseAddress(0);
-    interleaveData(endOfText);
-
-    FixDataRegionsPass fixDataRegions;
-    setup->getConductor()->getProgram()->accept(&fixDataRegions);
-
-    writeOut(pos);
-
-    setup->getConductor()->writeDebugElf("bin-symbols.elf");
-
-    LOG(0, "entry point is located at 0x"
-        << std::hex << setup->getEntryPoint());
-
-    return 0;
-}
 
 void BinGen::changeMapAddress(Module *module, address_t address) {
     auto map = module->getElfSpace()->getElfMap();
@@ -224,50 +260,78 @@ void BinGen::interleaveData(address_t pos) {
     LOG(1, "rouding up pos " << pos << " to " << ROUND_UP_BY(pos, 8));
     pos = ROUND_UP_BY(pos, 8);
     LOG(1, "data should start at " << pos);
-    LOG(1, "copying in main rodata");
     pos = copyInData(mainModule, pos, false);
     if(addon) {
         LOG(1, "rouding up pos " << pos << " to " << ROUND_UP_BY(pos, 8));
         pos = ROUND_UP_BY(pos, 8);
-        LOG(1, "copying in addon rodata");
         pos = copyInData(addon, pos, false);
     }
 
-    pos = ROUND_UP(pos);
-    LOG(1, "copying in main rwdata to box");
+    pos = ROUND_UP(pos);    // page align <-- different protection
     pos = copyInData(mainModule, pos, true);
     if(addon) {
         LOG(1, "rouding up pos " << pos << " to " << ROUND_UP_BY(pos, 8));
         pos = ROUND_UP_BY(pos, 8);
-        LOG(1, "copying in addon rwdata to box");
         pos = copyInData(addon, pos, true);
+    }
+
+    pos = ROUND_UP(pos);    // page align <-- different page
+    LOG(1, "remapping in bss to box");
+    pos = remapInBss(mainModule, pos);
+    if(addon) {
+        pos = remapInBss(addon, pos);
     }
 }
 
 address_t BinGen::copyInData(Module *module, address_t pos, bool writable) {
+    LOG(1, "copying in " << module->getName() << (writable ? "rw" : "ro")
+        << "data");
     for(auto region : CIter::children(module->getDataRegionList())) {
         if(region->writable() != writable) continue;
-        if(region->bss()) continue;
+        if(region->bssOnly()) {
+            // only the pointers that targets this region need to be fixed
+            continue;
+        }
 
-        LOG(1, "copying in to " << pos
+        auto dataSize = region->getDataSize();
+        auto bssSize = region->getBssSize();
+
+        LOG(1, "copying in to " << std::hex << pos
             << " from " << (region->getAddress() + region->getStartOffset()));
         std::memcpy((void *)pos,
             (void *)(region->getAddress() + region->getStartOffset()),
-            region->getSize() - region->getStartOffset());
-        //ChunkMutator(region).setPosition(pos - region->getStartOffset());
+            dataSize);
+        std::memset((void *)(pos + dataSize), 0, bssSize);
+
         LOG(1, "offset is " << region->getStartOffset());
         ChunkMutator(region).setPosition(pos - region->getStartOffset());
 
-        LOG(1, "base for " << region->getName() << " set to " << region->getAddress());
-        pos += region->getSize() - region->getStartOffset();
+        LOG(1, "base for " << region->getName()
+            << " set to " << region->getAddress());
+        pos += dataSize + bssSize;
     }
 
     return pos;
 }
 
+address_t BinGen::remapInBss(Module *module, address_t pos) {
+    LOG(1, "remapping " << module->getName() << " bss segments");
+    for(auto region : CIter::children(module->getDataRegionList())) {
+        if(region->bssOnly()) {
+            LOG0(1, "remap " << region->getAddress());
+            ChunkMutator(region).setPosition(pos);
+            LOG(1, " to " << region->getAddress());
+            pos += region->getSize();
+        }
+    }
+    return pos;
+}
+
 void BinGen::writeOut(address_t pos) {
+    LOG(1, "writing out main code");
     pos = writeOutCode(mainModule, pos);
     if(addon) {
+        LOG(1, "writing out addon code");
         pos = writeOutCode(addon, pos);
     }
 
@@ -289,7 +353,7 @@ void BinGen::writeOut(address_t pos) {
 
 // this needs to write out PLT too (if addon is a library)
 address_t BinGen::writeOutCode(Module *module, address_t pos) {
-    const int ll = 1;
+    const int ll = 10;
     std::vector<Function *> list;
     for(auto func : CIter::functions(module)) {
         list.push_back(func);
@@ -339,10 +403,11 @@ address_t BinGen::writeOutRwData(Module *module, address_t pos) {
 address_t BinGen::writeOutData(Module *module, address_t pos, bool writable) {
     for(auto region : CIter::children(module->getDataRegionList())) {
         if(region->writable() != writable) continue;
+        if(region->bssOnly()) continue;
 
-        LOG(1, "region at " << region->getAddress());
+        LOG(1, "region at " << std::hex << region->getAddress());
         LOG(1, "  offset " << region->getStartOffset());
-        LOG(1, "  size   " << region->getSize());
+        LOG(1, "  mem size " << region->getPhdr()->p_memsz);
         LOG(1, "pos at " << pos);
         auto start = region->getAddress() + region->getStartOffset();
         if(pos != start) {
@@ -351,7 +416,7 @@ address_t BinGen::writeOutData(Module *module, address_t pos, bool writable) {
             fs << zero;
             pos += start - pos;
         }
-        auto size = region->getSize() - region->getStartOffset();
+        auto size = region->getPhdr()->p_memsz - region->getStartOffset();
         LOG(1, "writing out data: " << region->getName()
             << " at " << start << " to " << (start + size));
         fs.write(reinterpret_cast<char *>(start), size);
--- src/generate/bingen.h
+++ src/generate/bingen.h
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <vector>
 #include "types.h"
+#include "chunk/dataregion.h"
 
 class ConductorSetup;
 class Module;
@@ -13,6 +14,7 @@ private:
     ConductorSetup *setup;
     Module *mainModule;
     Module *addon;
+    std::vector<Module *> moduleList;
     std::ofstream fs;
 public:
     BinGen(ConductorSetup *setup, const char *filename);
@@ -21,14 +23,17 @@ public:
     int generate();
 
 private:
+    void extractLinkerSymbols();
     void applyAdditionalTransform();
     void addCallLogging();
+    void addBssClear();
     void dePLT();
     address_t reassignFunctionAddress();
     address_t makeImageBox();
     void changeMapAddress(Module *module, address_t address);
     void interleaveData(address_t pos);
     address_t copyInData(Module *module, address_t pos, bool writable);
+    address_t remapInBss(Module *module, address_t pos);
     void writeOut(address_t pos);
     address_t writeOutCode(Module *module, address_t pos);
     address_t writeOutRoData(Module *module, address_t pos);
