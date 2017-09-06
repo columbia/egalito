1959a7421f0b0e59edc1bd87e70aaa50e72c0cc1
dwk <dwks42@gmail.com>
2017-09-06 11:25:01 -0400

    Add new config file for x86_64 Debian.

---
+++ config/x86_64-Debian.h
@@ -0,0 +1,18 @@
+#ifndef EGALITO_CONFIG_X86_64_DEBIAN_H
+#define EGALITO_CONFIG_X86_64_DEBIAN_H
+
+/* For Debian testing (buster) on X86_64 */
+
+/* common */
+
+/* src */
+
+#define SANDBOX_BASE_ADDRESS    0x40000000
+
+/* app */
+
+#define PROMPT_COLOR    C_WHITE
+
+/* test */
+
+#endif
