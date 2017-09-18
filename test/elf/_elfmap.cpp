7eb6459ddccab8ddad586a7fdc1c3d3ab80b25c5
Hidenori <hidenori.kobayashi@gmail.com>
2017-09-18 12:32:01 -0400

    Disable a test (ElfMap) for AARCH64.
    gcc7 on AARCH64 produces a DYN binary with -fPIE -pie -static.

--- test/elf/_elfmap.cpp
+++ test/elf/_elfmap.cpp
@@ -2,11 +2,14 @@
 #include "elf/elfmap.h"
 
 TEST_CASE("Elf Parser Dynamic", "[elf][dyn]") {
-	ElfMap elf(TESTDIR "hello");
-	CHECK(elf.isDynamic());
+    ElfMap elf(TESTDIR "hello");
+    CHECK(elf.isDynamic());
 }
 
 TEST_CASE("Elf Parser Executable", "[elf][exec]") {
-	ElfMap elf(TESTDIR "hello-static");
-	CHECK(elf.isExecutable());
+#ifdef ARCH_X86_64
+    // static PIE on AARCH64 is a shared object
+    ElfMap elf(TESTDIR "hello-static");
+    CHECK(elf.isExecutable());
+#endif
 }
