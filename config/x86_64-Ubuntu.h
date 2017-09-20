ead7933bfa63200b8bb51748634083a38bce0ad6
dwk <dwks42@gmail.com>
2017-09-20 11:54:32 -0400

    Config for building on Ubuntu x86_64.

---
+++ config/x86_64-Ubuntu.h
@@ -0,0 +1,19 @@
+#ifndef EGALITO_CONFIG_X86_64_UBUNTU_H
+#define EGALITO_CONFIG_X86_64_UBUNTU_H
+
+/* For Ubuntu 16.04 LTS on X86_64 */
+
+/* common */
+
+/* src */
+
+#define USR_LIB_DEBUG_BY_NAME
+#define SANDBOX_BASE_ADDRESS    0x40000000
+
+/* app */
+
+#define PROMPT_COLOR    C_GREEN
+
+/* test */
+
+#endif
