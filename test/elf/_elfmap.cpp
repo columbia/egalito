849697a1227edd6822e2ae710f22455234464c06
dwk <dwks42@gmail.com>
2017-09-26 16:44:20 -0400

    Remove test case based on hello-static. Fix Gentoo ./runner.
    It didn't run on aarch64 OpenSUSE or x86 Gentoo.

--- test/elf/_elfmap.cpp
+++ test/elf/_elfmap.cpp
@@ -5,11 +5,3 @@ TEST_CASE("Elf Parser Dynamic", "[elf][dyn]") {
     ElfMap elf(TESTDIR "hello");
     CHECK(elf.isDynamic());
 }
-
-TEST_CASE("Elf Parser Executable", "[elf][exec]") {
-#ifdef ARCH_X86_64
-    // static PIE on AARCH64 is a shared object
-    ElfMap elf(TESTDIR "hello-static");
-    CHECK(elf.isExecutable());
-#endif
-}
