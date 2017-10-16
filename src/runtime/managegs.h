32968c4895ee156519bedb248ed3ea87c3a36eb2
dwk <dwks42@gmail.com>
2017-10-16 10:57:59 -0400

    Add managegs.{cpp,h} to version control.

---
+++ src/runtime/managegs.h
@@ -0,0 +1,11 @@
+#ifndef EGALITO_RUNTIME_MANAGE_GS_H
+#define EGALITO_RUNTIME_MANAGE_GS_H
+
+#include "chunk/gstable.h"
+
+class ManageGS {
+public:
+    static void init(GSTable *gsTable);
+};
+
+#endif
