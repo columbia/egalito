b6b3b168019533a9a341a9249b0c4dd08c5fd275
Hidenori <hidenori.kobayashi@gmail.com>
2017-10-16 09:26:48 -0400

    Don't strip off the first NOP for functions with a symbol.

--- src/pass/removepadding.cpp
+++ src/pass/removepadding.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <capstone/capstone.h>
 #include "removepadding.h"
 #include "chunk/concrete.h"
@@ -5,16 +6,29 @@
 #include "operation/mutator.h"
 
 #include "log/log.h"
+#include "log/temp.h"
+
+void RemovePadding::visit(Module *module) {
+    //TemporaryLogLevel tll("pass", 10);
+    recurse(module);
+}
 
 // the name of the function may not match the starting address after this
 void RemovePadding::visit(Function *function) {
+#ifdef ARCH_AARCH64
     auto firstBlock = function->getChildren()->getIterable()->get(0);
     auto firstInstr = static_cast<Instruction *>(
         firstBlock->getChildren()->getIterable()->get(0));
     auto semantic = firstInstr->getSemantic();
     auto assembly = semantic->getAssembly();
-    if(assembly && assembly->getId() == ARM64_INS_NOP) {
+    if(!function->getSymbol()
+        && assembly && assembly->getId() == ARM64_INS_NOP) {
+
         LOG(10, function->getName() << ":    removing first NOP");
+        LOG(10, std::hex << firstInstr->getAddress());
+        // __GNUC__ >= 5 for AARCH64
+        assert(firstInstr->getAddress() % 8);
+
         auto nextInstr = static_cast<Instruction *>(
             firstBlock->getChildren()->getIterable()->get(1));
 
@@ -29,6 +43,7 @@ void RemovePadding::visit(Function *function) {
         }
     }
 
+    // this needs to run for functions whose size is estimated
     auto lastBlock = function->getChildren()->getIterable()->getLast();
     auto lastInstr = static_cast<Instruction *>(
         lastBlock->getChildren()->getIterable()->getLast());
@@ -45,4 +60,5 @@ void RemovePadding::visit(Function *function) {
             delete lastBlock;
         }
     }
+#endif
 }
--- src/pass/removepadding.h
+++ src/pass/removepadding.h
@@ -3,12 +3,14 @@
 
 #include "chunkpass.h"
 
+class Module;
 class FunctionList;
 class Function;
 
 class RemovePadding : public ChunkPass {
 public:
     RemovePadding() {}
+    virtual void visit(Module *module);
     virtual void visit(Function *function);
 private:
     void removePadding(FunctionList *functionList);
