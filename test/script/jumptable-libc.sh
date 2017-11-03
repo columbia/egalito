6c16dec187d9b58754ea68d40de8abb2cd413152
Hidenori <hidenori.kobayashi@gmail.com>
2017-11-03 10:54:06 -0400

    Enable libc jumptable test for AARCH64 again.

--- test/script/jumptable-libc.sh
+++ test/script/jumptable-libc.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+arch=$1
+
 mkdir tmp 2>/dev/null
 
 ../../app/etshell >tmp/jumptable-libc.out <<EOF
-parse ../binary/x86_64-debian/libc/libc.so.6
+parse ../binary/$arch/libc/libc.so.6
 jumptables
 EOF
 
-cp ../binary/x86_64-debian/libc/table-list.txt tmp/jumptable-libc.1
+cp ../binary/$arch/libc/table-list.txt tmp/jumptable-libc.1
 perl -ne 'print if(s/jump table in //)' tmp/jumptable-libc.out \
     | sed 's/ at.* with / has /' | sort > tmp/jumptable-libc.2
 
-./jumptable-diff.pl ../binary/x86_64-debian/libc/libc.so.6 \
+./jumptable-diff.pl ../binary/$arch/libc/libc.so.6 \
     tmp/jumptable-libc.1 tmp/jumptable-libc.2 | tee tmp/jumptable-libc.out
 
 if [ -z "$(cat tmp/jumptable-libc.out)" ]; then
