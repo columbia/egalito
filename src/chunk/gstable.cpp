9abedca0220c7f6e92504a0397e3466eb5b71f68
Hidenori <hidenori.kobayashi@gmail.com>
2017-12-04 15:57:14 -0500

    Allow a GSTableEntry to be pre-resolved.

--- src/chunk/gstable.cpp
+++ src/chunk/gstable.cpp
@@ -1,5 +1,7 @@
 #include <cassert>
 #include "gstable.h"
+#include "log/log.h"
+
 
 #define ENTRY_SIZE 8
 
@@ -11,17 +13,27 @@ void GSTableEntry::accept(ChunkVisitor *visitor) {
     // NYI
 }
 
-GSTableEntry *GSTable::makeEntryFor(Chunk *target) {
+GSTableEntry *GSTable::makeEntryFor(Chunk *target, bool preResolved) {
+    auto entry = getEntryFor(target);
+    if(!entry) {
+        if(preResolved) {
+            entry = new GSTableResolvedEntry(target, nextIndex());
+        }
+        else {
+            entry = new GSTableEntry(target, nextIndex());
+        }
+        getChildren()->add(entry);
+        entryMap[target] = entry;
+    }
+    return entry;
+}
+
+GSTableEntry *GSTable::getEntryFor(Chunk *target) {
     auto it = entryMap.find(target);
     if(it != entryMap.end()) {
         return (*it).second;
     }
-    else {
-        auto entry = new GSTableEntry(target, nextIndex());
-        getChildren()->add(entry);
-        entryMap[target] = entry;
-        return entry;
-    }
+    return nullptr;
 }
 
 GSTableEntry::IndexType GSTable::nextIndex() const {
@@ -36,6 +48,7 @@ GSTableEntry::IndexType GSTable::offsetToIndex(GSTableEntry::IndexType offset) {
 GSTableEntry *GSTable::getAtIndex(GSTableEntry::IndexType index) {
     index -= 1;
     if(index >= getChildren()->getIterable()->getCount()) {
+        LOG(0, "table overflow?");
         return nullptr;
     }
 
