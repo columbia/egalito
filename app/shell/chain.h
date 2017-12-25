ba2b582fefb062f2df20711cd2bdd55f1cbf4613
dwk <dwks42@gmail.com>
2017-12-24 18:11:08 -0700

    Introduce a "run" command in the shell that exec's the loader.

---
+++ app/shell/chain.h
@@ -0,0 +1,9 @@
+#ifndef EGALITO_SHELL_CHAIN_H
+#define EGALITO_SHELL_CHAIN_H
+
+#include "command.h"
+#include "conductor/setup.h"
+
+void executeLoader(ConductorSetup *setup, Arguments args);
+
+#endif
