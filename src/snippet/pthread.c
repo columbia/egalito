b1c7ab5c69e3160b08a4fa52d27342da73b8afe6
Hidenori <hidenori.kobayashi@gmail.com>
2017-12-26 14:26:44 -0500

    Add a wrapper to the start_routine of the spawned thread.

--- src/snippet/pthread.c
+++ src/snippet/pthread.c
@@ -1,11 +1,37 @@
+#include <assert.h>
 #include <pthread.h>
+#include <stdlib.h>
 #include "cminus/print.h"
+
+struct egalito_thread_arg {
+    void *(*start_routine)(void *);
+    void *arg;
+};
+
+void *egalito_thread_start(void *arg) {
+    struct egalito_thread_arg *egalito_arg = arg;
+    void *(*org_start_routine)(void *) = egalito_arg->start_routine;
+    void *org_arg = egalito_arg->arg;
+    free(arg);
+
+    // create a sandbox for this thread here
+
+    egalito_printf("calling original start_routine\n");
+    return org_start_routine(org_arg);
+}
 
 int egalito_pthread_create(void *thread, const void *attr,
     void *(*start_routine)(void *), void *arg) {
 
-    egalito_printf("calling egalito_pthread_create\n");
-    int status = pthread_create(thread, attr, start_routine, arg);
+    struct egalito_thread_arg *egalito_arg
+        = malloc(sizeof(struct egalito_thread_arg));
+    assert(egalito_arg);
+    egalito_arg->start_routine = start_routine;
+    egalito_arg->arg = arg;
+
+    egalito_printf("hijacking to egalito_pthread_create\n");
+    int status = pthread_create(thread, attr,
+        egalito_thread_start, egalito_arg);
 
     return status;
 }
