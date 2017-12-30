5da5b5f9aae6e66a5bc0bb68ea308ce3ee266f2c
Hidenori <hidenori.kobayashi@gmail.com>
2017-12-30 16:50:24 -0500

    Delete thread start routine wrapper.

--- src/snippet/pthread.cpp
+++ src/snippet/pthread.cpp
@@ -9,34 +9,11 @@
 
 extern ConductorSetup *egalito_conductor_setup;
 
-struct egalito_thread_arg {
-    void *(*start_routine)(void *);
-    void *arg;
-};
-
 extern "C" void egalito_jit_gs_transition(ShufflingSandbox *, GSTable *);
-
-extern "C"
-void *egalito_thread_start(void *arg) {
-    struct egalito_thread_arg *egalito_arg = (struct egalito_thread_arg *)arg;
-    void *(*org_start_routine)(void *) = egalito_arg->start_routine;
-    void *org_arg = egalito_arg->arg;
-    free(arg);
-
-    egalito_printf("calling original start_routine\n");
-    return org_start_routine(org_arg);
-}
 
 extern "C"
 int egalito_pthread_create(pthread_t *thread, const pthread_attr_t *attr,
     void *(*start_routine)(void *), void *arg) {
-
-    struct egalito_thread_arg *egalito_arg = (struct egalito_thread_arg *)
-        malloc(sizeof(struct egalito_thread_arg));
-
-    assert(egalito_arg);
-    egalito_arg->start_routine = start_routine;
-    egalito_arg->arg = arg;
 
     // do most of the preparation in the parent context to minimize the time
     // the child thread executes in the parent code space.
@@ -51,12 +28,14 @@ int egalito_pthread_create(pthread_t *thread, const pthread_attr_t *attr,
     EgalitoTLS::setChild(&child);
 
     //egalito_printf("hijacking to egalito_pthread_create\n");
-    int status = pthread_create(thread, attr,
-        egalito_thread_start, egalito_arg);
+
+    // we need a hook right after the clone syscall, but don't need
+    // a wrapper for the original thread start routine.
+    int status = pthread_create(thread, attr, start_routine, arg);
 
     EgalitoTLS::setChild(nullptr);
 
-    // we need to make sure that the parent does not reset the
+    // XXX: we need to make sure that the parent does not reset the
     // start_routine until the child has its set of resolver functions
 
     return status;
