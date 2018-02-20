ffbc0b6eb8ba6ef648a4699ba30c8dbfd3c9380e
Hidenori <hidenori.kobayashi@gmail.com>
2018-02-20 12:48:50 -0500

    Disable EncodingCheckPass for X86_64.

--- src/pass/encodingcheckpass.cpp
+++ src/pass/encodingcheckpass.cpp
@@ -3,15 +3,17 @@
 #include "log/log.h"
 
 void EncodingCheckPass::visit(Module *module) {
-    LOG(1, "CheckPass " << module->getName());
+    LOG(10, "CheckPass " << module->getName());
     recurse(module);
 }
 
 void EncodingCheckPass::visit(Instruction *instruction) {
+#ifdef ARCH_AARCH64
     if(auto linked = dynamic_cast<LinkedInstruction *>(
         instruction->getSemantic())) {
 
         LOG(10, "checking " << std::hex << instruction->getAddress());
         linked->check();
     }
+#endif
 }
