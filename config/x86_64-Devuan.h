ad5683f7b0bc9df2772ea7d84af2c7ce08dcb835
ethereal <ethereal@ethv.net>
2018-03-09 16:40:00 -0500

    Add config for Devuan.

---
+++ config/x86_64-Devuan.h
@@ -0,0 +1,23 @@
+#ifndef EGALITO_CONFIG_X86_64_DEBIAN_H
+#define EGALITO_CONFIG_X86_64_DEBIAN_H
+
+/* For Debian testing (buster) on X86_64 */
+
+/* common */
+
+/* src */
+
+#define USR_LIB_DEBUG_BY_HASH
+#define SANDBOX_BASE_ADDRESS    0x40000000
+#define JIT_TABLE_SIZE          64 * 0x1000 // must fit in 32-bit
+
+/* app */
+
+#define PROMPT_COLOR    C_WHITE
+
+/* test */
+
+#define ANALYSIS_JUMPTABLE_MAIN_COUNT               1
+#define ANALYSIS_JUMPTABLE_PARSE_EXPRESSION_COUNT   2
+
+#endif
