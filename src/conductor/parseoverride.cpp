86ec26b5defde87e85445426cad81c3050b26458
ethereal <ethereal@ethv.net>
2018-08-29 13:20:48 -0400

    Parse parse overrides from environment variable.
    Also, minor code cleanups.

--- src/conductor/parseoverride.cpp
+++ src/conductor/parseoverride.cpp
@@ -1,4 +1,11 @@
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 #include <fstream>
+#include <cstring>
 #include <sstream>
 
 #include "parseoverride.h"
@@ -24,19 +31,52 @@ BlockBoundaryOverride *BlockBoundaryOverride::parse(std::istream &stream,
 
 ParseOverride ParseOverride::instance;
 
+void ParseOverride::parseFromEnvironmentVar() {
+    const char *envp = getenv("EGALITO_PARSE_OVERRIDES");
+    if(!envp) return;
+    std::string env = envp;
+    if(env == "") return;
+
+    std::string::size_type start = 0;
+    while(true) {
+        std::string::size_type next = env.find(":", start);
+        parse(env.substr(start, next-start));
+        if(next == std::string::npos) break;
+        start = next+1;
+    }
+}
+
 void ParseOverride::parse(const std::string &from) {
-    parseFile(from);
+    struct stat s;
+    stat(from.c_str(), &s);
+    if(S_ISDIR(s.st_mode)) {
+        parseDir(from);
+    }
+    else {
+        parseFile(from);
+    }
+}
+
+void ParseOverride::parseDir(const std::string &dirname) {
+    DIR *dir = opendir(dirname.c_str());
+    struct dirent *dirent;
+    while((dirent = readdir(dir))) {
+        if(std::strcmp(dirent->d_name, ".") == 0) continue;
+        if(std::strcmp(dirent->d_name, "..") == 0) continue;
+        parse(dirname + "/" + dirent->d_name);
+    }
 }
 
 void ParseOverride::parseFile(const std::string &filename) {
     std::ifstream f(filename);
 
     if(!f) {
-        LOG(1, "Failed to parse overrides from \"" << filename << "\"");
+        LOG(1, "Failed to parse overrides from file \"" << filename << "\"");
         return;
     }
 
     // XXX: currently all numbers need to be hex
+    // example:
     // blockoverride "A" func
     //     0x0 0x50
     //     0x100 0x110
