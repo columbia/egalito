7a7702d330d29b7bc1a815965d9e91553cf29a0d
grahamp <gpp2109@columbia.edu>
2018-09-21 18:32:44 +0000

    Creating a common base class for Indirect{Call,Jump}Instruction.

--- src/instr/concrete.h
+++ src/instr/concrete.h
@@ -42,32 +42,45 @@ public:
     virtual void accept(InstructionVisitor *visitor) { visitor->visit(this); }
 };
 
-class JumpTable;
-class IndirectJumpInstruction : public IsolatedInstruction {
+class IndirectControlFlowInstructionBase : public IsolatedInstruction {
 private:
     Register reg;
-    std::string mnemonic;
     bool memory;
     Register index; // only relevant if memory
     size_t scale;   // only relevant if memory
     int64_t displacement;   // only relevant if memory
-    std::vector<JumpTable *> jumpTables;
 public:
-    IndirectJumpInstruction(Register reg, const std::string &mnemonic)
-        : reg(reg), mnemonic(mnemonic), memory(false),
+    IndirectControlFlowInstructionBase(Register reg)
+        : reg(reg), memory(false),
         index(INVALID_REGISTER), scale(1), displacement(0) {}
 
-    IndirectJumpInstruction(Register reg, const std::string &mnemonic,
+    IndirectControlFlowInstructionBase(Register reg,
         Register index, size_t scale, int64_t displacement)
-        : reg(reg), mnemonic(mnemonic), memory(true),
+        : reg(reg), memory(true),
         index(index), scale(scale), displacement(displacement) {}
 
-    std::string getMnemonic() const { return mnemonic; }
     Register getRegister() const { return reg; }
     bool hasMemoryOperand() const { return memory; }
     Register getIndexRegister() const { return index; }
     size_t getScale() const { return scale; }
     int64_t getDisplacement() const { return displacement; }
+};
+
+class JumpTable;
+class IndirectJumpInstruction : public IndirectControlFlowInstructionBase {
+private:
+    std::string mnemonic;
+    std::vector<JumpTable *> jumpTables;
+public:
+    IndirectJumpInstruction(Register reg, const std::string &mnemonic)
+        : IndirectControlFlowInstructionBase(reg), mnemonic(mnemonic) {}
+
+    IndirectJumpInstruction(Register reg, const std::string &mnemonic,
+        Register index, size_t scale, int64_t displacement)
+        : IndirectControlFlowInstructionBase(reg, index, scale, displacement),
+        mnemonic(mnemonic) {}
+
+    std::string getMnemonic() const { return mnemonic; }
 
     // After jump table passes have run, either the jumpTable pointer will be
     // set, or this jump has another purpose (e.g. indirect tail recursion).
@@ -78,28 +91,9 @@ public:
     virtual void accept(InstructionVisitor *visitor) { visitor->visit(this); }
 };
 
-class IndirectCallInstruction : public IsolatedInstruction {
-private:
-    Register reg;
-    bool memory;
-    Register index; // only relevant if memory
-    size_t scale;   // only relevant if memory
-    int64_t displacement;   // only relevant if memory
+class IndirectCallInstruction : public IndirectControlFlowInstructionBase {
 public:
-    IndirectCallInstruction(Register reg)
-        : reg(reg), memory(false),
-        index(INVALID_REGISTER), scale(1), displacement(0) {}
-
-    IndirectCallInstruction(Register reg,
-        Register index, size_t scale, int64_t displacement)
-        : reg(reg), memory(true),
-        index(index), scale(scale), displacement(displacement) {}
-
-    Register getRegister() const { return reg; }
-    bool hasMemoryOperand() const { return memory; }
-    Register getIndexRegister() const { return index; }
-    size_t getScale() const { return scale; }
-    int64_t getDisplacement() const { return displacement; }
+    using IndirectControlFlowInstructionBase::IndirectControlFlowInstructionBase;
 
     virtual void accept(InstructionVisitor *visitor) { visitor->visit(this); }
 };
