2aa5f5d25f0e2975093b9e01b36c6d58db0549ee
grahamp <gpp2109@columbia.edu>
2018-11-11 16:33:37 +0000

    Adding more ifuncs to the statically resolved list.

--- dep/ifunc/determine.c
+++ dep/ifunc/determine.c
@@ -17,16 +17,24 @@ int main(int argc, char **argv)
     uint8_t buf[100];
     uint8_t buf2[100];
     wchar_t wbuf[100];
+    wchar_t wbuf2[100];
     double d;
 
     memset(buf, 0, 10);
     memcpy(buf, buf2, 10);
+    __memcpy_chk(buf, buf2, 10);
     mempcpy(buf, buf2, 10);
     memmove(buf, buf2, 10);
     if(memcmp(buf, buf2, 11)) {
         nop();
     }
+    if(wmemcmp(wbuf, wbuf2, 11)) {
+        nop();
+    }
     if(memchr(buf, 0, 0) == NULL) {
+        nop();
+    }
+    if(wmemchr(wbuf, 0, 0) == NULL) {
         nop();
     }
     if(strcmp(buf, buf2)) {
@@ -41,6 +49,9 @@ int main(int argc, char **argv)
     if(strncasecmp(buf, buf2, 1)) {
         nop();
     }
+    if(strstr(buf, buf2)) {
+        nop();
+    }
     if(strlen(argv[0]) > 0) {
         nop();
     }
@@ -52,7 +63,9 @@ int main(int argc, char **argv)
     buf2[0] = 0;
     buf2[1] = 2;
     strcpy(buf, buf2);
+    strncpy(buf, buf2, 0);
     stpcpy(buf, buf2);
+    stpncpy(buf, buf2, 0);
     if(strchr(buf, 0) == NULL) {
         nop();
     }
@@ -68,6 +81,9 @@ int main(int argc, char **argv)
     if(strcspn(buf, buf2) == 0) {
         nop();
     }
+    if(strpbrk(buf, buf2) == 0) {
+        nop();
+    }
     {
         locale_t loc = newlocale(1 << LC_ALL, "C", NULL);
         if(strcasecmp_l(buf, buf2, loc) == 0) {
@@ -81,6 +97,9 @@ int main(int argc, char **argv)
     if(rawmemchr(buf, 0)) {
         nop();
     }
+    if(memrchr(buf, 0, 0)) {
+        nop();
+    }
 
     wmemset(wbuf, 0, 10);
     if(wcslen(wbuf) == 0) {
@@ -90,10 +109,29 @@ int main(int argc, char **argv)
         nop();
     }
     d = 0.5;
-    if(cos(d) > 0.1) {
-        nop();
-    }
-
+	double z; float zf;
+    if(cos(d) > 0.1) nop();
+    if(cosf(d) > 0.1) nop();
+    if(ceil(d) > 0.1) nop();
+    if(ceilf(d) > 0.1) nop();
+    if(floor(d) > 0.1) nop();
+    if(floorf(d) > 0.1) nop();
+    if(trunc(d) > 0.1) nop();
+    if(truncf(d) > 0.1) nop();
+    if(rint(d) > 0.1) nop();
+    if(rintf(d) > 0.1) nop();
+    if(tan(d) > 0.1) nop();
+    if(tanf(d) > 0.1) nop();
+    if(atan(d) > 0.1) nop();
+    if(atanf(d) > 0.1) nop();
+    sincos(d, &z, &z);
+    sincosf(d, &zf, &zf);
+    if(sin(d) > 0.1) nop();
+    if(sinf(d) > 0.1) nop();
+    if(exp(d) > 0.1) nop();
+    if(expf(d) > 0.1) nop();
+    if(log(d) > 0.1) nop();
+    if(logf(d) > 0.1) nop();
     breakpoint();
 
     return 0;
--- dep/ifunc/determine.py
+++ dep/ifunc/determine.py
@@ -9,7 +9,9 @@ class DetermineIFuncTarget (gdb.Command):
                 gdb.COMPLETE_NONE)
 
     def invoke (self, arg, from_tty):
-        ifuncs = ["memset", "memcpy", "mempcpy", "memmove", "memcmp", "memchr", "strcmp", "strncmp", "strcasecmp", "strlen", "strnlen", "strcpy", "stpcpy", "strchr", "strrchr", "strchrnul", "strspn", "strcspn", "strcat", "strcasecmp_l", "rawmemchr", "wmemset", "wcslen", "wcsnlen", "cos"]
+        #ifuncs = ["memset", "memcpy", "mempcpy", "memmove", "memcmp", "memchr", "strcmp", "strncmp", "strcasecmp", "strncasecmp", "strlen", "strnlen", "strcpy", "stpcpy", "strchr", "strrchr", "strchrnul", "strspn", "strcspn", "strcat", "strcasecmp_l", "rawmemchr", "wmemset", "wcslen", "wcsnlen", "cos"]
+        # ifuncs = ["cos", "memchr", "memcmp", "memcpy", "memmove", "mempcpy", "memset", "newlocale", "rawmemchr", "stpcpy", "strcasecmp", "strcasecmp_l", "strcat", "strchr", "strchrnul", "strcmp", "strcpy", "strcspn", "strlen", "strncasecmp", "strncmp", "strncpy", "strnlen", "strrchr", "strspn", "wcslen", "wcsnlen", "wmemset"]
+        ifuncs = [ "atan", "atanf", "ceil", "ceilf", "cos", "cosf", "exp", "expf", "floor", "floorf", "log", "logf", "memchr", "memcmp", "memcpy","__memcpy_chk", "memmove", "mempcpy", "memrchr", "memset", "newlocale", "rawmemchr", "rint", "rintf", "sin", "sincos", "sincosf", "sinf", "stpcpy", "stpncpy", "strcasecmp", "strcasecmp_l", "strcat", "strchr", "strchrnul", "strcmp", "strcpy", "strcspn", "strlen", "strncasecmp", "strncmp", "strncpy", "strnlen", "strpbrk", "strrchr", "strspn", "strstr", "tan", "tanf", "trunc", "truncf", "wcslen", "wcsnlen", "wmemchr", "wmemcmp", "wmemset" ]
         with open('ifunc.h', 'w') as f:
             for ifunc in ifuncs:
                 target = self.determine("'%s@plt'" % ifunc)
@@ -23,7 +25,10 @@ class DetermineIFuncTarget (gdb.Command):
             gdb.execute('set $pc = 0x%x' % value.address, False, True)
             gdb.execute('si', False, True)
             #print('PC = %x' % gdb.selected_frame().pc())
-            return gdb.selected_frame().name()
+            name = gdb.selected_frame().name()
+            if(name == 'memcpy'): name = '__memcpy_sse2_unaligned'
+            if(name == 'top12'): name = '__expf_sse2'
+            return name
 
 
 DetermineIFuncTarget()
