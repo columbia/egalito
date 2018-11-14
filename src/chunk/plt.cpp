1df966f27b3d934b4deb23239632ffbe7fa24e8f
ethereal <ethereal@ethv.net>
2018-11-14 14:32:51 -0500

    Properly find PLT trampoline targets.

--- src/chunk/plt.cpp
+++ src/chunk/plt.cpp
@@ -332,7 +332,7 @@ PLTList *PLTList::parse(RelocList *relocList, ElfMap *elf, Module *module) {
             }
         }
     }
-#else
+#elif defined(ARCH_AARCH64)
     static const size_t ENTRY_SIZE = 16;
 
     /* example format
@@ -372,6 +372,39 @@ PLTList *PLTList::parse(RelocList *relocList, ElfMap *elf, Module *module) {
                 pltList->getChildren()->add(
                     new PLTTrampoline(pltList, pltAddress, externalSymbol, value));
             }
+        }
+    }
+#elif defined(ARCH_RISCV)
+    static const size_t ENTRY_SIZE = 16;
+
+    /* Example PLT trampoline:
+   103b0:       00002e17                auipc   t3,0x2
+   103b4:       c60e3e03                ld      t3,-928(t3) # 12010 <puts@GLIBC_2.26>
+   103b8:       000e0367                jalr    t1,t3
+   103bc:       00000013                nop
+     */
+
+    for(size_t i = 2 * ENTRY_SIZE; i < header->sh_size; i += ENTRY_SIZE) {
+        auto entry = section + i;
+        address_t pltAddress = header->sh_addr + i;
+
+        // start with the 20-bit PC-relative immediate
+        address_t base =
+            pltAddress + (*reinterpret_cast<int32_t *>(entry) & ~0xfff);
+        int32_t offset = *reinterpret_cast<int32_t *>(entry + 4);
+        offset >>= 20;
+
+        address_t value = base + offset;
+        LOG(1, "VALUE might be " << value);
+        Reloc *r = registry->find(value);
+        if(r && r->getSymbol()) {
+            LOG(1, "Found PLT entry at " << pltAddress << " -> ["
+                << r->getSymbolName() << "]");
+
+            auto externalSymbol = ExternalSymbolFactory(module)
+                .makeExternalSymbol(r->getSymbol());
+            pltList->getChildren()->add(
+                new PLTTrampoline(pltList, pltAddress, externalSymbol, value));
         }
     }
 #endif
