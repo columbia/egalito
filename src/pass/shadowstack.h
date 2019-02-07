5e6c4649c345f71520fe03f0c81855714cf04610
Nick <njb2132@columbia.edu>
2019-02-07 13:27:11 -0500

    Forgot to add shadowstack.{cpp,h}.

---
+++ src/pass/shadowstack.h
@@ -0,0 +1,31 @@
+#ifndef EGALITO_PASS_SHADOW_STACK_H
+#define EGALITO_PASS_SHADOW_STACK_H
+
+#include "chunkpass.h"
+
+class ShadowStackPass : public ChunkPass {
+public:
+    enum Mode {
+        MODE_CONST,
+        MODE_GS,
+    };
+private:
+    Mode mode;
+    Function *violationTarget;
+public:
+    ShadowStackPass(Mode mode = MODE_CONST) : mode(mode),
+        violationTarget(nullptr) {}
+    virtual void visit(Module *module);
+    virtual void visit(Function *function);
+    virtual void visit(Instruction *instruction);
+private:
+    void pushToShadowStack(Function *function);
+    void pushToShadowStackConst(Function *function);
+    void pushToShadowStackGS(Function *function);
+    void popFromShadowStack(Instruction *instruction);
+    void popFromShadowStackConst(Instruction *instruction);
+    void popFromShadowStackGS(Instruction *instruction);
+};
+
+
+#endif
