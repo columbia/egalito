791259cdd7038882b6efdafe8c057454ac045805
David Williams-King <dwk@redpill.cs.columbia.edu>
2019-02-07 14:29:40 -0500

    Introducing new etcet executable for endbr CFI + shadow stack.

---
+++ app/cet/etcet.h
@@ -0,0 +1,4 @@
+#ifndef EGALITO_APP_SANDBOX_H
+#define EGALITO_APP_SANDBOX_H
+
+#endif
---
+++ app/cet/inject/cet.c
@@ -0,0 +1,26 @@
+#define _GNU_SOURCE
+#include <asm/prctl.h>
+#include <sys/prctl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#define SHADOW_STACK_SIZE (12*1024*1024)
+
+int arch_prctl(int code, void *addr);
+
+__attribute__ (( __constructor__ ))
+void allocate_shadow_stack(void) {
+    void *memory = mmap(0,
+        SHADOW_STACK_SIZE,
+        PROT_READ | PROT_WRITE,
+        MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+
+    // layout:
+    //   [0] = ptr
+    //   [1] = null
+    //   [2] = top of stack
+    unsigned long *data = memory;
+    data[0] = 2*sizeof(unsigned long);
+
+    arch_prctl(ARCH_SET_GS, memory);
+}
