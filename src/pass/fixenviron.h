7e3074ddcc860e81433e15b3176ca9e368e871cf
David Williams-King <dwk@redpill.cs.columbia.edu>
2019-03-12 11:07:18 -0400

    Update FixEnvironPass to work after endbrenforce (fix etharden -u --cfi).

--- src/pass/fixenviron.h
+++ src/pass/fixenviron.h
@@ -3,6 +3,9 @@
 
 #include "chunkpass.h"
 
+/** Add code to set __environ variable in _start, for uniongen only.
+    This variable is normally set in _start for static executables.
+*/
 class FixEnvironPass : public ChunkPass {
 public:
     virtual void visit(Program *program);
