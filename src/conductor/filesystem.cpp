e427cb35cdbfbacb8f924107a6999da28e5750b2
ethereal <ethereal@ethv.net>
2020-02-11 15:33:41 -0500

    Fix debug symbol location by .debug-link.

--- src/conductor/filesystem.cpp
+++ src/conductor/filesystem.cpp
@@ -2,6 +2,8 @@
 
 #include "filesystem.h"
 
+#include "log/log.h"
+
 ConductorFilesystem::ConductorFilesystem() {
     const char *csysroot = getenv("EGALITO_SYSROOT");
     if(csysroot) sysroot = csysroot;
@@ -14,4 +16,12 @@ std::string ConductorFilesystem::transform(const std::string &path) {
     if(path[0] == '/') return sysroot + path;
     // don't touch relative paths
     else return path;
+}
+
+std::string ConductorFilesystem::untransform(const std::string &path) {
+    if(path.substr(0, sysroot.size()) == sysroot) return path.substr(sysroot.size());
+    else {
+        LOG(1, "unsure how to untransform path [" << path << "]");
+        return path;
+    }
 }
