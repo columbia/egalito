6578cf0780f3d0b1b517fde2671283352cfc27fb
vidya <vidyalakshmi.raj@gmail.com>
2021-05-17 20:59:32 -0400

    Cleaned up logs-2

--- src/pass/syspartpass.cpp
+++ src/pass/syspartpass.cpp
@@ -39,61 +39,6 @@ void SyspartPass::visit(Module *module) {
 
 #endif
 }
-/*
-void SyspartPass::visit(Function *func)
-{
-
-    
-	if((func->getName() == this->function->getName()) && (func->getAddress() == this->function->getAddress()))
-	{
-		for(auto bl : CIter::children(this->function))
-		{
-			for(auto instr : CIter::children(bl))
-			{
-				if(instr->getAddress() == this->address)
-				{
-                    auto block2 = new Block();
-                    //PositionFactory *positionFactory = PositionFactory::getInstance();
-                    //block2->setPosition(positionFactory->makePosition(bl, block2, bl->getSize()));
-                    ChunkMutator m1(block2);
-
-					auto call = new Instruction();
-					auto callSem = new ControlFlowInstruction(
-            X86_INS_CALL, call, "\xe8", "call", 4);
-                    auto module = (Module*)func->getParent()->getParent();
-                    auto newRef = CIter::named(module->getPLTList())->find(enforcement_func->getName());
-                    if(newRef)
-                    {
-                        LOG(1, "YEAH PLT ENTRY FOUND");
-                        callSem->setLink(new PLTLink(newRef->getAddress(), newRef));
-                    }
-                    else
-                    {
-                        LOG(1, "NAAH PLT ENTRY NOT FOUND");
-            		    callSem->setLink(new NormalLink(enforcement_func, Link::SCOPE_EXTERNAL_JUMP));
-                    }
-            		call->setSemantic(callSem);
-                    m1.append(call);
-                    ChunkMutator m2(func);
-                    m2.insertAfter(bl, block2); */
-                    /*
-            		{
-            			ChunkMutator m(bl, true);
-            			//m.prepend(call);
-                        if(before)
-            			     m.insertBeforeJumpTo(instr, call);
-                         else
-            			     m.append(call);
-
-            			return;
-            		}*/
-                    /*
-				}
-			}
-		}
-	}
-}
-*/
 
 void SyspartPass::visit(Function *func)
 {
@@ -112,22 +57,7 @@ void SyspartPass::visit(Function *func)
                 auto last =  (Instruction*)bl->getChildren()->genericGetLast();
                 if(auto cfi = dynamic_cast<ControlFlowInstruction *>(last->getSemantic())) 
                 {
-                    //cout<<"CFI at "<< std::hex<<last->getAddress()<<endl;
                     auto sem1 = last->getSemantic();
-                    /*
-		    if(auto linked = dynamic_cast<LinkedInstructionBase *>(sem1))
-                    {
-                        cout<<"LINKED INSTRUCTION BASE"<<endl;
-                    }
-                    if(auto linked = dynamic_cast<ControlFlowInstructionBase *>(sem1))
-                    {
-                        cout<<"CONTROL FLOW INSTRUCTION BASE "<<(linked->getSource())->getAddress()<<endl;
-                    }
-                    if((cfi->getMnemonic())[0] == 'j')
-                    {
-                       cout<<"JUMP HERE"<<endl;
-                    }
-		    */
                 }
                 m1.insertBefore(last, call);
 
@@ -145,42 +75,19 @@ void SyspartPass::visit(Function *func)
             callSem->setLink(new NormalLink(enforcement_func, Link::SCOPE_EXTERNAL_JUMP));
             call->setSemantic(callSem);
             m2.append(call);
-            //cout<<"Appended call"<<endl;
             m1.insertAfter(previous_sibling, block2);
-            //m1.insertBefore(previous_sibling, block2);
-            //auto last_instr = previous_sibling->getChildren()->genericGetLast();
-            //cout<<"Previous sibling "<<previous_sibling->getAddress()<<" "<<last_instr->getAddress()<<endl;
-
-            //ChunkMutator m1(previous_sibling);
-            //m1.append(call);
-            //m1.insertAfter(last_instr, call);
-            cout<<"Added block"<<endl;
+            
             for(auto bl : non_loop_parents)
             {
-                //if(bl->getAddress() == previous_sibling->getAddress())
-                //    continue;
+
                 auto last =  (Instruction*)bl->getChildren()->genericGetLast();
                 if(auto cfi = dynamic_cast<ControlFlowInstruction *>(last->getSemantic())) 
                 {
-                    cout<<"CFI at "<< std::hex<<last->getAddress()<<endl;
                     auto sem1 = last->getSemantic();
-                    if(auto linked = dynamic_cast<LinkedInstructionBase *>(sem1))
-                    {
-                        cout<<"LINKED INSTRUCTION BASE"<<endl;
-                    }
-                    if(auto linked = dynamic_cast<ControlFlowInstructionBase *>(sem1))
-                    {
-                        cout<<"CONTROL FLOW INSTRUCTION BASE "<<(linked->getSource())->getAddress()<<endl;
-                    }
                     if((cfi->getMnemonic())[0] == 'j')
                     {
                         auto link = last->getSemantic()->getLink();
                         last->getSemantic()->setLink(new NormalLink(call, link->getScope()));
-                        //auto sem1 = last->getSemantic();
-                        //if(auto linked = dynamic_cast<LinkedInstructionBase *>(sem1))
-                            //linked->setInstruction(call);
-                        //if(auto linked = dynamic_cast<ControlFlowInstructionBase *>(sem1))
-                            //linked->setSource(call);
                     }
                 }
             }
